05d6c499f9fa2d9c5e45ee9183bb7a7d36128791
seraermolenko <sera.ermolenko@gmail.com>
2025-02-25 16:29:21 -0800

    keybpard Interrupt catch

--- dummyValues.py
+++ dummyValues.py
@@ -7,16 +7,23 @@ import os
 load_dotenv() 
 api_url = os.getenv('API_URL')
 
+def main():
+    while True:
+        humidity = random.randint(30, 80)
+        data = {
+            "sensor_id": 1,
+            "humidity": humidity
+        }
+        response = requests.post(api_url, json=data)
+        if response.status_code == 200:
+            print(f"Data sent: {data}")
+        else:
+            print(f"Error sending data: {response.status_code}")
+        time.sleep(2)  # every 2 sec send data 
 
-while True:
-    humidity = random.randint(30, 80)
-    data = {
-        "sensor_id": 1,
-        "humidity": humidity
-    }
-    response = requests.post(api_url, json=data)
-    if response.status_code == 200:
-        print(f"Data sent: {data}")
-    else:
-        print(f"Error sending data: {response.status_code}")
-    time.sleep(2)  # every 2 sec send data 
+
+if __name__ == "__main__": 
+    try: 
+        main()
+    except KeyboardInterrupt: 
+        pass
